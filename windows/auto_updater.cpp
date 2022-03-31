26a686a8ecd4dbd6de110d095f16c918fdc83511
wangyunxiu <020142103>
2022-03-31 10:20:28 +0800

    格式化代码

--- windows/auto_updater.cpp
+++ windows/auto_updater.cpp
@@ -26,11 +26,11 @@ void AutoUpdater::SetFeedURL(std::string feedURL) {
 }
 
 void AutoUpdater::CheckForUpdates() {
-        win_sparkle_check_update_with_ui();
+  win_sparkle_check_update_with_ui();
 }
 
 void AutoUpdater::CheckForUpdatesWithoutUI() {
-        win_sparkle_check_update_without_ui();
+  win_sparkle_check_update_without_ui();
 }
 
 }  // namespace
